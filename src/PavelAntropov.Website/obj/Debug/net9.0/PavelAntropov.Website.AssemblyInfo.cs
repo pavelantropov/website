875d64e76e837c7421b93c3f5f26c50f207fe771
Pavel Antropov <p.m.antropov@gmail.com>
2026-09-06 12:54:51 +0400

    add caddyfile and docker-compose.prod

--- src/PavelAntropov.Website/obj/Debug/net9.0/PavelAntropov.Website.AssemblyInfo.cs
+++ src/PavelAntropov.Website/obj/Debug/net9.0/PavelAntropov.Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PavelAntropov.Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0490820b640f2f18666924401e14c86d2808e8fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7b31cc843e65f7849e7cf90fcbfb7590de127b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PavelAntropov.Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PavelAntropov.Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
